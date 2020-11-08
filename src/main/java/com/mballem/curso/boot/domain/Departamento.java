970eb2f730f00ca4ddfa8a2723fdf29f4bef1629
Carlosouzapsi <carlosouzapsi@gmail.com>
2020-11-08 13:05:40 -0300

    Inserindo o inicio dos controllers

---
+++ src/main/java/com/mballem/curso/boot/domain/Departamento.java
@@ -0,0 +1,31 @@
+package com.mballem.curso.boot.domain;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@SuppressWarnings("serial")
+@Entity
+@Table(name = "DEPARTAMENTOS")
+public class Departamento extends AbstractEntity<Long>{
+	
+	@Column(name = "nome", nullable = false, unique = true, length = 60)
+	private String nome;
+	
+	@OneToMany(mappedBy = "departamento")
+	private List<Cargo> cargos;
+	
+	public String GetNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+
+}
