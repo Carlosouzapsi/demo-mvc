cc576839863d188a9631e119e22e6646533fb44a
Carlosouzapsi <carlosouzapsi@gmail.com>
2020-11-10 13:07:25 -0300

    inserir-departamentos-implementado

--- src/main/java/com/mballem/curso/boot/web/controller/DepartamentoController.java
+++ src/main/java/com/mballem/curso/boot/web/controller/DepartamentoController.java
@@ -1,15 +1,23 @@
 package com.mballem.curso.boot.web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.mballem.curso.boot.domain.Departamento;
+import com.mballem.curso.boot.service.DepartamentoService;
 
 @Controller
 @RequestMapping("/departamentos")
 public class DepartamentoController {
 	
+	@Autowired
+	private DepartamentoService service;
+	
 	@GetMapping("/cadastrar")
-	public String cadastrar() {
+	public String cadastrar(Departamento departamento) {
 		return "/departamento/cadastro";
 	}
 	
@@ -18,6 +26,10 @@ public class DepartamentoController {
 		return "/departamento/lista";
 	}
 	
+	@PostMapping("/salvar")
+	public String salvar(Departamento departamento) {
+		service.salvar(departamento);
+		return "redirect:/departamentos/cadastrar";
+	}
 	
-
 }
